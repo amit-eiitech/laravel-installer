d9574c481f4148d3b3ca753280845978663f43e0
vincenzo vecchio <vince.karibusana@gmail.com>
2026-01-10 21:19:52 +0000

    Add loading to steps
    1. Add loading to button steps, for example after setting database credentials it can take some time to load next step, the button will show loading… status to inform the user that something is happening.
    
    Signed-off-by: vincenzo vecchio <vince.karibusana@gmail.com>

--- src/resources/views/livewire/install/installer-wizard.blade.php
+++ src/resources/views/livewire/install/installer-wizard.blade.php
@@ -49,12 +49,43 @@
 
                 <div class="mt-auto flex justify-end gap-3">
                     @if ($currentIndex === $steps->count() - 1)
-                        <button class="btn-primary mt-12" wire:click="completeStep">
-                            Finish
+                        <button 
+                            class="btn-primary mt-12 flex items-center justify-center gap-2 disabled:opacity-70 disabled:cursor-wait" 
+                            wire:click="completeStep"
+                            wire:loading.attr="disabled"
+                            wire:target="completeStep"
+                        >
+                            <span wire:loading.remove wire:target="completeStep">
+                                Finish
+                            </span>
+                            
+                            <span wire:loading.flex wire:target="completeStep" class="items-center gap-2">
+                                <svg class="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                                    <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                </svg>
+                                Installing...
+                            </span>
                         </button>
                     @else
-                        <button class="btn-primary mt-12" wire:click="completeStep" @disabled(!$canProceed)>
-                            Next
+                        <button 
+                            class="btn-primary mt-12 flex items-center justify-center gap-2 disabled:opacity-70 disabled:cursor-wait" 
+                            wire:click="completeStep" 
+                            wire:loading.attr="disabled"
+                            wire:target="completeStep"
+                            @disabled(!$canProceed)
+                        >
+                            <span wire:loading.remove wire:target="completeStep">
+                                Next
+                            </span>
+
+                            <span wire:loading.flex wire:target="completeStep" class="items-center gap-2">
+                                <svg class="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                                    <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                </svg>
+                                Loading...
+                            </span>
                         </button>
                     @endif
                 </div>
